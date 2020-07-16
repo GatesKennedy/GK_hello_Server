08e2ab59a166cc21f9775b977c50dfefebac5b5f
GatesKennedy <conor@esccollective.com>
2020-07-16 00:24:11 -0700

    MOD: db scripts - seed script tables

--- db_______/scripts/seed.sql
+++ db_______/scripts/seed.sql
@@ -2,12 +2,17 @@
 --  db_chat     ohnodamn    utf8
 --88888888 88888888 88888888 88888888 88888888 88888888 88888888 88888888 
 
-DROP TABLE IF EXISTS tbl_user CASCADE;      -- 0
+DROP TABLE IF EXISTS tbl_user           CASCADE;      -- 0
+DROP TABLE IF EXISTS tbl_profile        CASCADE;      -- 0
+DROP TABLE IF EXISTS tbl_prof_history   CASCADE;      -- 0
+DROP TABLE IF EXISTS tbl_talk           CASCADE;      -- 0
+DROP TABLE IF EXISTS tbl_talk_history   CASCADE;      -- 0
+DROP TABLE IF EXISTS tbl_collab         CASCADE;      -- 0
 
 --  tool
 CREATE EXTENSION IF NOT EXISTS "uuid-ossp";
 --  user
-CREATE TYPE entity_type AS ENUM ('personal', 'biz_sml', 'biz_med', 'biz_lrg');
+CREATE TYPE entity_type AS ENUM ('void', 'personal', 'biz_sml', 'biz_med', 'biz_lrg');
 CREATE TYPE role_type   AS ENUM ('admin', 'dev', 'user');
 CREATE TYPE field_type  AS ENUM ('thought, puzzle, entity, img_url');
 CREATE TYPE share_type  AS ENUM ('text', 'image', 'audio', 'link', 'collab');
@@ -22,7 +27,6 @@ CREATE TABLE IF NOT EXISTS tbl_user(
     date_join   date        NOT NULL DEFAULT CURRENT_DATE,
     PRIMARY KEY (id)
 );
-
 CREATE TABLE IF NOT EXISTS tbl_profile(
     user_id         UUID            REFERENCES tbl_user(id) ON DELETE CASCADE,
     entity          entity_type     NOT NULL DEFAULT 'void',
@@ -32,7 +36,6 @@ CREATE TABLE IF NOT EXISTS tbl_profile(
     img_url         TEXT            NOT NULL DEFAULT 'NO_URL',
     location VARCHAR(50)
 );
-
 CREATE TABLE IF NOT EXISTS tbl_prof_history(
     id          SERIAL,
     user_id     UUID        REFERENCES tbl_user(id) ON DELETE CASCADE,
@@ -41,8 +44,6 @@ CREATE TABLE IF NOT EXISTS tbl_prof_history(
     edit_note   TEXT        NOT NULL DEFAULT 'NO_ENTRY',        --(-_-)--
     PRIMARY KEY (id)
 );
-
-
 CREATE TABLE IF NOT EXISTS tbl_talk(
     id          UUID        DEFAULT uuid_generate_v4(),
     give_id     UUID        REFERENCES tbl_user(id) ON DELETE CASCADE,
@@ -60,7 +61,6 @@ CREATE TABLE IF NOT EXISTS tbl_talk_history(
     edit_note   TEXT        NOT NULL DEFAULT 'NO_ENTRY',        --(-_-)--
     PRIMARY KEY (id)
 );
-
 CREATE TABLE IF NOT EXISTS tbl_collab(
     id          SERIAL,
     seed_id     UUID        REFERENCES tbl_user(id) ON DELETE CASCADE,
@@ -76,7 +76,7 @@ CREATE TABLE IF NOT EXISTS tbl_collab(
 --  .sql Script from CMD
 --===============================
 --  psql -U user_name -d db_name -a -f <file_path>
---  psql -U ohnodamn -d db_nds -f C:\Programming\Gates_Kennedy\GK_hello\GK_hello_Server\db_______\scripts\seed.sql
+--  psql -U ohnodamn -d db_talk -f C:\Programming\Gates_Kennedy\GK_hello\GK_hello_Server\db_______\scripts\seed.sql
 --  ~Heroku~
 --  Not Connected to Heroku psql:
 --      cat <file_name> | heroku pg:psql
