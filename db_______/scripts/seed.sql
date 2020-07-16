b665406d2f3d6943841950f231f9e3172ec636dc
GatesKennedy <conor@esccollective.com>
2020-07-16 02:00:04 -0700

    MOD: db scripts - seed script populates initial table design

--- db_______/scripts/seed.sql
+++ db_______/scripts/seed.sql
@@ -2,19 +2,26 @@
 --  db_chat     ohnodamn    utf8
 --88888888 88888888 88888888 88888888 88888888 88888888 88888888 88888888 
 
-DROP TABLE IF EXISTS tbl_user           CASCADE;      -- 0
+DROP TABLE IF EXISTS tbl_user           CASCADE;      -- 3
+DROP TABLE IF EXISTS tbl_talk           CASCADE;      -- 2
 DROP TABLE IF EXISTS tbl_profile        CASCADE;      -- 0
 DROP TABLE IF EXISTS tbl_prof_history   CASCADE;      -- 0
-DROP TABLE IF EXISTS tbl_talk           CASCADE;      -- 0
 DROP TABLE IF EXISTS tbl_talk_history   CASCADE;      -- 0
-DROP TABLE IF EXISTS tbl_collab         CASCADE;      -- 0
+DROP TABLE IF EXISTS tbl_access         CASCADE;      -- 0
+
+DROP TYPE IF EXISTS entity_type;
+DROP TYPE IF EXISTS role_type;
+DROP TYPE IF EXISTS field_type;
+DROP TYPE IF EXISTS talk_type;
+DROP TYPE IF EXISTS share_type;
 
 --  tool
 CREATE EXTENSION IF NOT EXISTS "uuid-ossp";
 --  user
 CREATE TYPE entity_type AS ENUM ('void', 'personal', 'biz_sml', 'biz_med', 'biz_lrg');
 CREATE TYPE role_type   AS ENUM ('admin', 'dev', 'user');
-CREATE TYPE field_type  AS ENUM ('thought, puzzle, entity, img_url');
+CREATE TYPE field_type  AS ENUM ('thought', 'puzzle', 'entity', 'img_url');
+CREATE TYPE talk_type   AS ENUM ('chat', 'collab');
 CREATE TYPE share_type  AS ENUM ('text', 'image', 'audio', 'link', 'collab');
 
 CREATE TABLE IF NOT EXISTS tbl_user(
@@ -22,56 +29,50 @@ CREATE TABLE IF NOT EXISTS tbl_user(
     name        VARCHAR(16) NOT NULL,
     email       VARCHAR(64) NOT NULL,
     password    VARCHAR(64) NOT NULL,
-    business    VARCHAR(32) NOT NULL DEFAULT 'void',
     role        role_type   NOT NULL DEFAULT 'user',
     date_join   date        NOT NULL DEFAULT CURRENT_DATE,
     PRIMARY KEY (id)
 );
 CREATE TABLE IF NOT EXISTS tbl_profile(
-    user_id         UUID            REFERENCES tbl_user(id) ON DELETE CASCADE,
+    user_id         UUID            REFERENCES tbl_user(id) NOT NULL,
     entity          entity_type     NOT NULL DEFAULT 'void',
-    website         VARCHAR(128)    NOT NULL DEFAULT 'void',
-    thought         VARCHAR         NOT NULL DEFAULT 'void',
-    puzzle          TEXT            NOT NULL DEFAULT 'void',
-    img_url         TEXT            NOT NULL DEFAULT 'NO_URL',
-    location VARCHAR(50)
+    website         VARCHAR(128),
+    thought         VARCHAR,
+    puzzle          TEXT,
+    img_url         TEXT,
+    location        VARCHAR(50)
 );
 CREATE TABLE IF NOT EXISTS tbl_prof_history(
     id          SERIAL,
-    user_id     UUID        REFERENCES tbl_user(id) ON DELETE CASCADE,
+    user_id     UUID        REFERENCES tbl_user(id) NOT NULL,
     field       field_type  NOT NULL, 
-    date_edited DATE        NOT NULL DEFAULT CURRENT_DATE,
-    edit_note   TEXT        NOT NULL DEFAULT 'NO_ENTRY',        --(-_-)--
+    body        TEXT        NOT NULL DEFAULT 'void',
+    date_edit   DATE        NOT NULL DEFAULT CURRENT_DATE,
     PRIMARY KEY (id)
 );
+
 CREATE TABLE IF NOT EXISTS tbl_talk(
     id          UUID        DEFAULT uuid_generate_v4(),
-    give_id     UUID        REFERENCES tbl_user(id) ON DELETE CASCADE,
-    take_id     UUID        REFERENCES tbl_user(id) ON DELETE CASCADE,
-    date_init  DATE        NOT NULL DEFAULT CURRENT_DATE,
+    type        talk_type   NOT NULL,
+    seen        BOOLEAN     NOT NULL DEFAULT true,
+    date_init   DATE        NOT NULL DEFAULT CURRENT_DATE,
     PRIMARY KEY (id)
 );
 CREATE TABLE IF NOT EXISTS tbl_talk_history(
     id          SERIAL,
-    talk_id     REFERENCES  tbl_talk(id),
+    talk_id     UUID        REFERENCES tbl_talk(id) NOT NULL,  --  INDEX
     type        share_type  NOT NULL, 
     body        JSONB       NOT NULL,
-    seen       BOOLEAN      NOT NULL DEFAULT true,
-    date        DATE        NOT NULL DEFAULT CURRENT_DATE,
+    seen        BOOLEAN     NOT NULL DEFAULT true,
+    date_edit   DATE        NOT NULL DEFAULT CURRENT_DATE,      --  INDEX
     edit_note   TEXT        NOT NULL DEFAULT 'NO_ENTRY',        --(-_-)--
     PRIMARY KEY (id)
 );
-CREATE TABLE IF NOT EXISTS tbl_collab(
-    id          SERIAL,
-    seed_id     UUID        REFERENCES tbl_user(id) ON DELETE CASCADE,
-    more_id     JSONB       REFERENCES tbl_user(id) ON DELETE CASCADE,
-    seen        BOOLEAN     NOT NULL DEFAULT true,
-    date_edited DATE        NOT NULL DEFAULT CURRENT_DATE,
-    edit_note   TEXT        NOT NULL DEFAULT 'NO_ENTRY',
-    PRIMARY KEY (id)
+CREATE TABLE IF NOT EXISTS tbl_access(
+    user_id     UUID        REFERENCES tbl_user(id) NOT NULL,   --  INDEX
+    talk_id     UUID        REFERENCES tbl_talk(id) NOT NULL,
+    PRIMARY KEY (user_id, talk_id)
 );
-
-
 
 --  .sql Script from CMD
 --===============================
