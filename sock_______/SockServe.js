5aef8e28235dadfd98000f9ec291e6a8402f40ad
GatesKennedy <conor@esccollective.com>
2020-07-21 15:20:22 -0700

    NEW: Sock Folder - SockServe.js, RoomServe.js (handling event logic), HelpServe.js (Helper Methods), Boiler Groundwork

---
+++ sock_______/SockServe.js
@@ -0,0 +1,31 @@
+const sockServe = require('http').createServer();
+const io = require('socket.io')(sockServe);
+
+io.on('connection', function (client) {
+  client.on('register', handleRegister);
+
+  client.on('join', handleJoin);
+
+  client.on('leave', handleLeave);
+
+  client.on('message', handleMessage);
+
+  client.on('chatrooms', handleGetChatrooms);
+
+  client.on('availableUsers', handleGetAvailableUsers);
+
+  client.on('disconnect', function () {
+    console.log('client disconnect...', client.id);
+    handleDisconnect();
+  });
+
+  client.on('error', function (err) {
+    console.log('received error from client:', client.id);
+    console.log(err);
+  });
+});
+
+sockServe.listen(3000, function (err) {
+  if (err) throw err;
+  console.log('listening on port 3000');
+});
