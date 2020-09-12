7e28ce89d68c44b16dd8b33358e9f501787239e1
GatesKennedy <conor@esccollective.com>
2020-09-12 15:07:19 -0700

    MOD: API > middleware - async await has util fxn refactor ; Error Handling - catch all returns diferent types of errors for 'development' and 'production' environments ;;

--- api_______/middleware/errorGen.js
+++ api_______/middleware/errorGen.js
@@ -1,7 +1,45 @@
-const serv = require('serv')
+const serv = require('serv');
 const { json } = require('express');
 
+// serv.use(err, req, res, next) => {
 
-serv.use(err, req, res, next) => {
+// };
 
+const catchAsync = (fxn) => {
+  return (req, res, next) => {
+    // fxn(req, res, next).catch((err) => next(err));
+    fxn(req, res, next).catch(next);
+  };
+};
+
+//  Error Types
+const sendErrorDev = (err, res) => {
+  res.status(err.statusCode).json({
+    status: err.status,
+    error: err,
+    message: err.message,
+    stack: err.stack,
+  });
+};
+
+const sendErrorProd = (err, res) => {
+  res.status(err.statusCode).json({
+    status: err.status,
+    message: err.message,
+  });
+};
+
+//   module.exports = (err, req, res, next) => {
+//     err.statusCode = err.statusCode || 500;
+//     err.status = err.status || 'error';
+
+//     if (process.env.NODE_ENV === 'development') {
+//       sendErrorDev(err, res);
+//     } else if (process.env.NODE_ENV === 'production') {
+//       sendErrorProd(err, res);
+//     }
+//   };
+
+module.exports = {
+  catchAsync,
 };
--- server.js
+++ server.js
@@ -101,6 +101,12 @@ serv.use((err, req, res, next) => {
   err.statusCode = err.statusCode || 500;
   err.status = err.status || 'Oops.. error';
 
+  if (process.env.NODE_ENV === 'development') {
+    sendErrorDev(err, res);
+  } else if (process.env.NODE_ENV === 'production') {
+    sendErrorProd(err, res);
+  }
+
   res.status(err.statusCode).json({
     status: err.status,
     message: err.message,
