23b21f32b4cf1ba7f97afe38cd8a18e73b5aaf50
GatesKennedy <conor@esccollective.com>
2021-01-12 02:39:11 -0800

    MOD: api > test.js - new route for testing db connection @ /api/test/db-async;;

--- api_______/api/test.js
+++ api_______/api/test.js
@@ -13,4 +13,32 @@ router.get('/', (req, res, next) => {
   res.json({ msg: `Good for ${NODE_ENV}...` });
 });
 
+router.get('/db-async', async (request, response, next) => {
+  console.log('(^=^) Enter FXN > GET: api/test/db-async');
+  try {
+    //  Async db Connection
+    const client = await pool.connect();
+    const queryText = `
+  SELECT
+   *
+  FROM tbl_user 
+  LIMIT 1
+  `;
+    const { rows } = await client.query(queryText);
+    await client.query('COMMIT');
+    // Successful Response
+    response.status(200).json(rows);
+  } catch (err) {
+    const errStr = JSON.stringify(err);
+    console.log(
+      '|     (>_<) api/test/db-async > FAIL Catch > errStr: ',
+      errStr
+    );
+    await client.query('ROLLBACK');
+    return next(err);
+  } finally {
+    client.release();
+  }
+});
+
 module.exports = router;
